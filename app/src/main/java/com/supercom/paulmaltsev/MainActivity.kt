71180c7cc14408a2f3a696a1a7b13355772ee858
Pavel Maltsev <38934377+PavelMaltsev20@users.noreply.github.com>
2023-03-17 13:12:28 +0200

    Add navigation to main activity

--- app/src/main/java/com/supercom/paulmaltsev/MainActivity.kt
+++ app/src/main/java/com/supercom/paulmaltsev/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.supercom.paulmaltsev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.supercom.paulmaltsev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.activityMain_container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNavigationViewListener()
+    }
+
+    private fun initBottomNavigationViewListener() {
+        binding.activityMainBnv.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuMain_map -> navController.navigate(R.id.action_global_mapFragment)
+                R.id.menuMain_bluetooth -> navController.navigate(R.id.action_global_bluetoothFragment)
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
